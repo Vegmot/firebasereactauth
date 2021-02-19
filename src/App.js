7e98a158f1edd662b49f60138522d222007933c6
Vegmot <anthony.yh.jang@gmail.com>
2021-02-19 15:48:48 -0800

    working on register/login/dashboard page, setting up firebase and context

--- src/App.js
+++ src/App.js
@@ -5,24 +5,27 @@ import HomeScreen from './components/home/HomeScreen';
 import Dashboard from './components/dashboard/Dashboard';
 import Login from './components/login/Login';
 import Register from './components/register/Register';
+import { AuthProvider } from './contexts/AuthContext';
 
 import './App.css';
 
 const App = () => {
   return (
     <>
-      <Router>
-        <Container>
-          <main>
-            <Switch>
-              <Route exact path='/' component={HomeScreen} />
-              <Route exact path='/dashboard' component={Dashboard} />
-              <Route exact path='/login' component={Login} />
-              <Route exact path='/register' component={Register} />
-            </Switch>
-          </main>
-        </Container>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Container>
+            <main>
+              <Switch>
+                <Route exact path='/' component={HomeScreen} />
+                <Route exact path='/dashboard' component={Dashboard} />
+                <Route exact path='/login' component={Login} />
+                <Route exact path='/register' component={Register} />
+              </Switch>
+            </main>
+          </Container>
+        </Router>
+      </AuthProvider>
     </>
   );
 };
--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Card, Form, Button, Alert } from 'react-bootstrap';
+import { useAuth } from '../../contexts/AuthContext';
 
 const Register = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+  const { register } = useAuth();
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const submitHandler = e => {
+    e.preventDefault();
+
+    if (passwordRef.current.value !== confirmPasswordRef.current.value) {
+      return setError('Passwords do not match');
+    }
+
+    try {
+      setError('');
+      setLoading(true);
+      register(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      setError('Failed to create an account');
+    }
+    setLoading(false);
+  };
+
   return (
     <>
-      <h1>Register</h1>
+      <Card>
+        <Card.Body>
+          <h2 className='text-center mb-4'>Sign up</h2>
+          {error && <Alert variant='danger'>{error}</Alert>}
+          <Form onSubmit={submitHandler}>
+            <Form.Group id='email'>
+              <Form.Label>Email</Form.Label>
+              <Form.Control type='email' required ref={emailRef} />
+            </Form.Group>
+
+            <Form.Group id='password'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control type='password' required ref={passwordRef} />
+            </Form.Group>
+
+            <Form.Group id='confirmPassword'>
+              <Form.Label>Confirm password</Form.Label>
+              <Form.Control type='password' required ref={confirmPasswordRef} />
+            </Form.Group>
+
+            <Button disabled={loading} type='submit' className='btn btn-block'>
+              Sign up
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+
+      <div className='w-100 text-center mt-2'>
+        Already have an account? <Link to='/login'>Login</Link>
+      </div>
     </>
   );
 };
